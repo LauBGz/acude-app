c323938cf6cca9cfa72606e8ed1ac62ac84f6620
LauBGz <msg13@outlook.es>
2020-03-17 20:35:55 +0100

    añadida vista principal

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const RouterConfig: Routes = [
+    {"path": "", "component": MainComponent},
+    {"path":"home", "component": MainComponent},
+  //{"path":"**", "component": }
+  ];  
+
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(RouterConfig, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
